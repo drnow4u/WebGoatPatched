e449d4d09753d5f0bb472bca0156e51b7a079696
Marcin Nowak <marcin.nowak@techmetria.pl>
2023-12-21 10:06:01 +0100

    Content Security Policy (CSP)

--- src/it/java/org/owasp/webgoat/CSPIntegrationTest.java
+++ src/it/java/org/owasp/webgoat/CSPIntegrationTest.java
@@ -6,27 +6,23 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class CSPIntegrationTest extends IntegrationTest {
 
-    @ParameterizedTest
-    @ValueSource(strings = {
-            "/SpoofCookie/login",
-            "/challenge/logo",
-            "/InsecureLogin/task"
-    })
-    void shouldPostResponseContainsCSPHeaders(String url) {
-        RestAssured.given()
-                .when()
-                .relaxedHTTPSValidation()
-//                .cookie("JSESSIONID", getWebGoatCookie())
-//                .formParam("username", getUser())
-//                .formParam("password", "password")
-                .post(url(url))
-                .then()
-//                .statusCode(302)
-//                .header("Location", containsString("/login"))
-                .header("referrer-policy", "strict-origin")
-                .header("content-security-policy", "style-src 'self' 'unsafe-inline'; worker-src 'none'; child-src 'none'; script-src 'self'; frame-src 'self' blob:; connect-src 'self'; img-src 'self' data:; default-src 'self'; base-uri 'self'; object-src 'none'; frame-ancestors 'none'; form-action 'self'; block-all-mixed-content; upgrade-insecure-requests;")
-                .header("x-content-type-options", "nosniff")
-                .header("x-frame-options", "DENY")
-                .header("x-xss-protection", "1; mode=block");
-    }
+  @ParameterizedTest
+  @ValueSource(strings = {"/SpoofCookie/login", "/challenge/logo", "/InsecureLogin/task"})
+  void shouldPostResponseContainsCSPHeaders(String url) {
+    RestAssured.given()
+        .when()
+        .relaxedHTTPSValidation()
+        .post(url(url))
+        .then()
+        .header("referrer-policy", "strict-origin")
+        .header(
+            "content-security-policy",
+            "style-src 'self' 'unsafe-inline'; worker-src 'none'; child-src 'none'; script-src"
+                + " 'self'; frame-src 'self' blob:; connect-src 'self'; img-src 'self' data:;"
+                + " default-src 'self'; base-uri 'self'; object-src 'none'; frame-ancestors 'none';"
+                + " form-action 'self'; block-all-mixed-content; upgrade-insecure-requests;")
+        .header("x-content-type-options", "nosniff")
+        .header("x-frame-options", "DENY")
+        .header("x-xss-protection", "1; mode=block");
+  }
 }
