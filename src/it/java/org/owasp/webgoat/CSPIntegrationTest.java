e6c44781f4d410f862a1db3f7f7b024afb10e867
Marcin Nowak <marcin.nowak@techmetria.pl>
2023-12-21 13:48:16 +0100

    Content Security Policy (CSP) (#1)
    * Content Security Policy (CSP)
    
    * Content Security Policy (CSP)
    
    * Content Security Policy (CSP)
    
    * Content Security Policy (CSP)
    
    * Content Security Policy (CSP)
    
    * Content Security Policy (CSP)
    
    * Content Security Policy (CSP)

---
+++ src/it/java/org/owasp/webgoat/CSPIntegrationTest.java
@@ -0,0 +1,28 @@
+package org.owasp.webgoat;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CSPIntegrationTest extends IntegrationTest {
+
+  @ParameterizedTest
+  @ValueSource(strings = {"/SpoofCookie/login", "/challenge/logo", "/InsecureLogin/task"})
+  void shouldPostResponseContainsCSPHeaders(String url) {
+    RestAssured.given()
+        .when()
+        .relaxedHTTPSValidation()
+        .post(url(url))
+        .then()
+        .header("referrer-policy", "strict-origin")
+        .header(
+            "content-security-policy",
+            "style-src 'self' 'unsafe-inline'; worker-src 'none'; child-src 'none'; script-src"
+                + " 'self'; frame-src 'self' blob:; connect-src 'self'; img-src 'self' data:;"
+                + " default-src 'self'; base-uri 'self'; object-src 'none'; frame-ancestors 'none';"
+                + " form-action 'self'; block-all-mixed-content; upgrade-insecure-requests;")
+        .header("x-content-type-options", "nosniff")
+        .header("x-frame-options", "DENY")
+        .header("x-xss-protection", "1; mode=block");
+  }
+}
---
+++ src/main/java/org/owasp/webgoat/CspPolicyConfig.java
@@ -0,0 +1,75 @@
+package org.owasp.webgoat;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+
+/**
+ * Content Security Policy (CSP) is an added layer of security that helps to detect and mitigate
+ * certain types of attacks, including Cross Site Scripting (XSS) and data injection attacks. These
+ * attacks are used for everything from data theft to site defacement or distribution of malware.
+ * CSP provides a set of standard HTTP headers that allow website owners to declare approved sources
+ * of content that browsers should be allowed to load on that page — covered types are JavaScript,
+ * CSS, HTML frames, fonts, images and embeddable objects such as Java applets, ActiveX, audio and
+ * video files.
+ *
+ * <p>CWE-693: Protection Mechanism Failure https://cwe.mitre.org/data/definitions/693.html
+ *
+ * <p>WASC-15: http://projects.webappsec.org/w/page/13246914/Application%20Misconfiguration
+ *
+ * <p>Solution: Ensure that your web server, application server, load balancer, etc. is configured
+ * to set the Content-Security-Policy header.
+ *
+ * @see <a
+ *     href="https://developer.mozilla.org/en-US/docs/Web/Security/CSP/Introducing_Content_Security_Policy">Content
+ *     Security Policy (CSP)</a> <a
+ *     href="https://cheatsheetseries.owasp.org/cheatsheets/Content_Security_Policy_Cheat_Sheet.html">Content
+ *     Security Policy Cheat Sheet</a> <a href="http://www.w3.org/TR/CSP/">Content Security Policy
+ *     Level 3</a> <a
+ *     href="http://www.html5rocks.com/en/tutorials/security/content-security-policy/">Content
+ *     security policy</a> <a href="http://content-security-policy.com/">Content Security Policy
+ *     Reference</a>
+ */
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class CspPolicyConfig {
+
+  private static final int LEAP_YEAR_IN_SECONDS = 366 * 24 * 60 * 60;
+  private static final String CSP_POLICY =
+      String.join(
+          " ",
+          "style-src 'self' 'unsafe-inline';",
+          "worker-src 'none';",
+          "child-src 'none';",
+          "script-src 'self';",
+          "frame-src 'self' blob:;",
+          "connect-src 'self';",
+          "img-src 'self' data:;",
+          "default-src 'self';",
+          "base-uri 'self';",
+          "object-src 'none';",
+          "frame-ancestors 'none';",
+          "form-action 'self';",
+          "block-all-mixed-content;",
+          "upgrade-insecure-requests;");
+
+  public static void setupSecurityHeaders(HttpSecurity http) throws Exception {
+    http.headers()
+        .httpStrictTransportSecurity()
+        .includeSubDomains(true)
+        .maxAgeInSeconds(LEAP_YEAR_IN_SECONDS)
+        .and()
+        .xssProtection()
+        .xssProtectionEnabled(true)
+        .block(true)
+        .and()
+        .contentSecurityPolicy(CSP_POLICY)
+        .and()
+        .contentTypeOptions()
+        .and()
+        .frameOptions()
+        .deny()
+        .cacheControl()
+        .and()
+        .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN);
+  }
+}
